cc59b607be9b55daf670e8a917d7a9c4be93a2de
dun1a <donyaa@metropolia.fi>
2024-10-04 18:50:09 +0300

    usa + japan kysymys

--- GameDir/game.py
+++ GameDir/game.py
@@ -180,12 +180,14 @@ else:
 # Afganistan
 
 def get_airport2():
-    sql = """Select iso_country, ident, name, latitude_deg, longitude_deg
-        from airport
+    sql = """SELECT country.name, airport.iso_country, airport.ident, airport.name, airport.latitude_deg, airport.longitude_deg
+        from country
+        left join airport
+        on airport.iso_country = country.iso_country 
         where ident = 'OAKB'"""
     cursor = conn.cursor(dictionary=True)
     cursor.execute(sql)
-    result = cursor.fetchall()
+    result = cursor.fetchall() # kannattaako nää fetchall muuttaa fetchone vai ei?
     return result
 
 airport = get_airport_info(current_airport)
@@ -209,19 +211,23 @@ current_airport = dest
 input('\033[32mPress Enter to continue...\033[0m')
 
 # afganistanin kysymys
-vastaus2 = input("Kuinka monta prosenttia maailman päästöistä syntyy lennoista? A) 15% B) 0,5-1% C) 2-3% : ")
+vastaus2 = input("Kuinka monta prosenttia maailman päästöistä syntyy lennoista? a) 15% b) 0,5-1% c) 2-3% : ")
 if vastaus2 == 'c' or 'C':
     print("Vastasit oikein.")
     budget += 500
     print(f"Tämän hetkinen budjettisi on {budget}.")
 else:
-    print("Vastasit väärin, oikea vastaus on C) 2-3%.")
+    print("Vastasit väärin, oikea vastaus on c) 2-3%.")
     print(f'Tämän hetkinen budjettisi on {budget}.')
+#________________________________________________________
 
+# Japan
 def get_airport3():
-    sql = """SELECT iso_country, ident, name, latitude_deg, longitude_deg
-        FROM airport
-        WHERE ident = 'RJAA'"""
+    sql = """SELECT country.name, airport.iso_country, airport.ident, airport.name, airport.latitude_deg, airport.longitude_deg
+        from country
+        left join airport
+        on airport.iso_country = country.iso_country 
+        where ident ='RJAA'"""
     cursor = conn.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -248,14 +254,15 @@ current_airport = dest
 input('\033[32mPress Enter to continue...\033[0m')
 
 #japanin kysymys
-vastaus3 = input("Kysymys? A) vastaus B) vastaus : ")
-if vastaus3 == 'a' or 'A':
+vastaus3 = input("Mikä on yhden henkilön CO2-päästöt lentomatkalla Tokiosta Dubliniin? a) 1336.5kg b) 1335.6kg c) 1563.3kg vastaus : ")
+if vastaus3 == 'b' or 'B':
     print("Vastasit oikein.")
     budget += 500
     print(f"Tämän hetkinen budjettisi on {budget}")
 else:
-    print("Vastasit väärin, oikea vastaus on A) vastaus.")
+    print("Vastasit väärin, oikea vastaus on b) 1335.6kg.")
     print(f"Tämän hetkinen budjettisi on {budget}.")
+#___________________________________________________________________-
 
 # yhdysvallat
 def get_airport4():
@@ -288,13 +295,13 @@ current_airport = dest
 input('\033[32mPress Enter to continue...\033[0m')
 
 #usan kysymys
-vastaus4 = input("Kysymys? A) vastaus B) vastaus : ")
+vastaus4 = input("Kuinka paljon hiilidioksidia syntyy yhdestä kilosta kerosiinia polttaessa? a) 3.16kg b) 0.54kg c) 2.7kgA) vastaus B) vastaus : ")
 if vastaus4 == 'a' or 'A':
     print("Vastasit oikein.")
     budget += 500
     print(f"Tämän hetkinen budjettisi on {budget}")
 else:
-    print("Vastasit väärin, oikea vastaus on A) vastaus.")
+    print("Vastasit väärin, oikea vastaus on a) 3.16kg.")
     print(f"Tämän hetkinen budjettisi on {budget}.")
 
 #formerly in the game loop (eu part), removed since u can't win in the europe part of the game
